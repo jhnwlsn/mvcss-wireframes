4d7de0e5faf1352edbb50ca932d3eadb8b6daa20
John Wilson <john@j-wilson.com>
2014-12-16 14:15:59 -0500

    Initial commit

---
+++ config.rb
@@ -0,0 +1,47 @@
+# --------------------------------------
+#   Config
+# --------------------------------------
+
+# ----- Middleman ----- #
+
+activate :livereload
+
+# ----- Assets ----- #
+
+set :css_dir, 'assets/stylesheets'
+set :js_dir, 'assets/javascripts'
+set :images_dir, 'assets/images'
+set :fonts_dir, 'assets/fonts'
+
+# ----- Images ----- #
+
+activate :automatic_image_sizes
+
+# --------------------------------------
+#   Content
+# --------------------------------------
+
+# ----- Directories for assets ----- #
+# ----- (needs to be activated after blog) ----- #
+
+activate :directory_indexes
+
+# --------------------------------------
+#   Production
+# --------------------------------------
+
+# ----- Prefixing ----- #
+
+activate :autoprefixer do |config|
+  config.browsers = ['last 2 versions', 'Explorer >= 9']
+  config.cascade = false
+end
+
+# ----- Build ----- #
+
+configure :build do
+  activate :minify_css
+  activate :minify_javascript
+  activate :relative_assets
+  activate :gzip
+end
