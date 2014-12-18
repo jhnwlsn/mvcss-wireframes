bc00b98dd8709a9a0aaf7fe84c15b19d92159727
John Wilson <john@j-wilson.com>
2014-12-18 12:15:38 -0500

    Create list partial controled by a helper

--- helpers/list_helpers.rb
+++ helpers/list_helpers.rb
@@ -1,9 +1,9 @@
 module ListHelpers
   def list_type(type)
     if type == "unordered"
-      partial "shared/list-items"
+      partial "shared/list", locals: {type: type}
     elsif type == "ordered"
-      "Ordered"
+      partial "shared/list", locals: {type: type}
     end
   end
 end
