742c4c454df888c1ae965bf759a62a2a8d54502c
John Wilson <john@j-wilson.com>
2014-12-18 15:37:55 -0500

    Oraganize existing shared partials

--- helpers/list_helpers.rb
+++ helpers/list_helpers.rb
@@ -1,9 +1,9 @@
 module ListHelpers
   def list_type(type)
     if type == "unordered"
-      partial "shared/list", locals: {type: type}
+      partial "shared/lists/list", locals: {type: type}
     elsif type == "ordered"
-      partial "shared/list", locals: {type: type}
+      partial "shared/lists/list", locals: {type: type}
     end
   end
 end
