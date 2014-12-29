b58c094c33e2fea123f8e975eb4301b4e9238a56
John Wilson <john@j-wilson.com>
2014-12-29 10:19:05 -0500

    Add split nav helpers into desktop and mobile

--- helpers/nav_helpers.rb
+++ helpers/nav_helpers.rb
@@ -1,15 +1,23 @@
 module NavHelpers
   def nav_content
-    'content--right' if ENV['NAVIGATION_LAYOUT'] == "left"
+    'content--right' if ENV['DESKTOP_LAYOUT'] == "left"
   end
 
-  def nav_render
-    if ENV['NAVIGATION_LAYOUT'] == "left"
+  def desktop_nav_render
+    if ENV['DESKTOP_LAYOUT'] == "left"
       "Render left navigation partial"
-    elsif ENV['NAVIGATION_LAYOUT'] == "top"
+    elsif ENV['DESKTOP_LAYOUT'] == "top"
       partial 'shared/navs/top'
     else
-      "Please include a proper navigation layout option in the .env file."
+      "Please include a proper desktop / tablet navigation layout option in the .env file."
+    end
+  end
+
+  def mobile_nav_render
+    if ENV['MOBILE_LAYOUT'] == "top-toggle"
+      "Render top toggle navigation partial"
+    else
+      "Please include a proper mobile navigation layout option in the .env file."
     end
   end
 end
